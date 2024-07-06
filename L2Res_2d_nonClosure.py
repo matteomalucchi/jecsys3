9dc746cf51f3397a183c5279e397d3b850ae4374
matteomalucchi <matteomalucchi13@gmail.com>
2024-07-06 13:30:48 +0200

    2d L2 non closure

--- L2Res_2d_nonClosure.py
+++ L2Res_2d_nonClosure.py
@@ -5,68 +5,75 @@ import sys
 import argparse
 import awkward as ak
 import scipy.ndimage
+import ROOT
+import ctypes
+import json
 
 hep.style.use("CMS")
 
 
-eta_bins_edges = [
-    0.0,
-    0.261,
-    0.522,
-    0.783,
-    1.044,
-    1.305,
-    1.479,
-    1.653,
-    1.93,
-    2.172,
-    2.322,
-    2.5,
-    2.65,
-    2.853,
-    2.964,
-    3.139,
-    3.314,
-    3.489,
-    3.839,
-    4.013,
-    4.583,
-    5.191,
-]
+eta_bins_edges = np.array(
+    [
+        0.0,
+        0.261,
+        0.522,
+        0.783,
+        1.044,
+        1.305,
+        1.479,
+        1.653,
+        1.93,
+        2.172,
+        2.322,
+        2.5,
+        2.65,
+        2.853,
+        2.964,
+        3.139,
+        3.314,
+        3.489,
+        3.839,
+        4.013,
+        4.583,
+        # 5.191,
+    ]
+)
 
-pt_bins_edges = [
-    15,
-    21,
-    28,
-    37,
-    49,
-    59,
-    86,
-    110,
-    132,
-    170,
-    204,
-    236,
-    279,
-    302,
-    373,
-    460,
-    575,
-    638,
-    737,
-    846,
-    967,
-    1101,
-    1248,
-    1410,
-    1588,
-    1784,
-    2000,
-    2238,
-    2500,
-    2787,
-    3103,
-]
+pt_bins_edges = np.array(
+    [
+        # 15,
+        # 21,
+        # 28,
+        # 37,
+        49,
+        59,
+        86,
+        110,
+        132,
+        170,
+        204,
+        236,
+        279,
+        302,
+        373,
+        460,
+        575,
+        638,
+        737,
+        846,
+        967,
+        1101,
+        1248,
+        1410,
+        1588,
+        1784,
+        2000,
+        2238,
+        2500,
+        2787,
+        3103,
+    ]
+)
 
 parser = argparse.ArgumentParser(description="Run the jme analysis")
 parser.add_argument(
@@ -83,43 +90,84 @@ parser.add_argument(
 )
 args = parser.parse_args()
 
+era_dict={
+    "2023Cv123":" Era C (early)"
+}
 
 def plot_2d_hist(histo_2d, fig_name):
     fig, ax = plt.subplots()
     ax.set_yscale("log")
-    hep.hist2dplot(histo_2d)
+    hep.hist2dplot(histo_2d, cbar=True, ax=ax, cmin=0.0, cmax=0.205)
 
     hep.cms.lumitext(
-        "2023 (13.6 TeV)",
+        f"{era_dict[args.era]} (13.6 TeV)",
+        fontsize=24,
     )
     hep.cms.text(
         text="SimulationPreliminary",
         loc=0,
+        fontsize=24,
     )
-    ax.set_ylabel(r"$p_{T}$ (GeV)", loc="top")
-    ax.set_xlabel(r"|$\eta$|", loc="right")
-    # ax.text(
-    #     0.3,
-    #     1,
-    #     r"anti-$k_{T}$ R=0.4 (PUPPI)",
-    #     horizontalalignment="left",
-    #     verticalalignment="top",
-    #     transform=ax.transAxes,
-    # )
-    # ax.text(
-    #     0.5,
-    #     1,
-    #     "PNet incl. neutrinos" if "neutrinos" in args.version else "PNet",
-    #     horizontalalignment="left",
-    #     verticalalignment="top",
-    #     transform=ax.transAxes,
-    # )
+    ax.set_ylabel(r"$p_{T}^{reco}$ (GeV)", loc="top")
+    ax.set_xlabel(r"|$\eta^{reco}$|", loc="right")
+    ax.text(
+        0.95,
+        0.95,
+        (
+            r"anti-$k_{T}$ R=0.4 (PUPPI)"
+            + ("\nPNet incl. neutrinos" if "neutrino" in args.version else "\nPNet")
+            + "\n"
+            + (
+                r"$\sigma_{L2Res}$"
+                if "Unc" not in fig_name
+                else r"$\sigma_{L2Res}  \oplus  \sigma_{L3Res}$"
+            )
+        ),
+        horizontalalignment="right",
+        verticalalignment="top",
+        transform=ax.transAxes,
+    )
 
     plt.savefig(
-        f"textfiles/L2Res_{args.version}/{fig_name}.png",
+        f"textfiles/L2Res_{args.version}/{fig_name}_{args.version}.png",
         bbox_inches="tight",
         dpi=300,
     )
+    plt.savefig(
+        f"textfiles/L2Res_{args.version}/{fig_name}_{args.version}.pdf",
+        bbox_inches="tight",
+        dpi=300,
+    )
+
+    # convert the numpy array in a c array
+    eta_bins=histo_2d[1].ctypes.data_as(ctypes.POINTER(ctypes.c_double))
+    pt_bins=histo_2d[2].ctypes.data_as(ctypes.POINTER(ctypes.c_double))
+
+    # dump histo in TH2D
+    hist_root = ROOT.TH2D(
+        f"{fig_name}_{args.version}",
+        f"{fig_name}_{args.version}",
+        len(histo_2d[1]) - 1,
+        eta_bins,
+        len(histo_2d[2]) - 1,
+        pt_bins,
+    )
+    for i in range(len(histo_2d[1]) - 1):
+        for j in range(len(histo_2d[2]) - 1):
+            hist_root.SetBinContent(i + 1, j + 1, histo_2d[0][i][j])
+
+    c = ROOT.TCanvas()
+    c.cd()
+    hist_root.Draw("colz")
+    c.Update()
+    c.Print(f"textfiles/L2Res_{args.version}/root_{fig_name}_{args.version}.png")
+    # save the histogram in a root file
+    file = ROOT.TFile(
+        f"textfiles/L2Res_{args.version}/{fig_name}_{args.version}.root", "RECREATE"
+    )
+    c.Write()
+    hist_root.Write()
+    file.Close()
 
 
 # load txt file
@@ -133,7 +181,6 @@ pt_bins_low = data[1, :]
 
 eta_unique = np.unique(eta_bins_low)
 
-print(type(pt_bins_low))
 # get the index where pt_bins is 15
 eta_bin_index = list(np.squeeze(np.argwhere(pt_bins_low == 15))) + [len(pt_bins_low)]
 
@@ -146,25 +193,60 @@ jes = ak.unflatten(data[2, :], eta_bin_lenght)
 eta_bins_low = ak.unflatten(eta_bins_low, eta_bin_lenght)
 pt_bins_low = ak.unflatten(pt_bins_low, eta_bin_lenght)
 
-for i in range(len(jes)):
-    print(eta_bins_low[i])
-    print(pt_bins_low[i])
-    print(jes[i])
-
 
 # pad the arrays with zeros to have the same lenght
 max_lenght = max(eta_bin_lenght)
 jes = ak.fill_none(ak.pad_none(jes, max_lenght, clip=True), 0) - 1
-jes = np.where(jes == -1, np.nan, jes)
-eta_bin_low = ak.fill_none(ak.pad_none(eta_bins_low, max_lenght, clip=True), 0)
-pt_bins_low = ak.fill_none(ak.pad_none(pt_bins_low, max_lenght, clip=True), 0)
+print(len(jes), len(jes[0]))
+jes = jes[:, 4:]
+print(len(jes), len(jes[0]))
+jes = ak.to_numpy(abs(np.where(jes == -1, np.nan, jes))[:-1])
 
+print(len(jes), len(jes[0]), type(jes))
+print(len(eta_bins_edges))
+print(len(pt_bins_edges))
 
 histo_2d = (jes, eta_bins_edges, pt_bins_edges)
 plot_2d_hist(histo_2d, f"L2Res_summary_{args.era}")
 
 
-# do the same histogram 2d but smoothing with gaussian filter
-jes_smooth = scipy.ndimage.gaussian_filter(jes, sigma=1)
-histo_2d_smooth = (jes_smooth, eta_bins_edges, pt_bins_edges)
-plot_2d_hist(histo_2d_smooth, f"L2Res_summary_smooth_{args.era}")
+unc_dict = {
+    # get the index of pt_bin_edges where it is over 200 for the first time
+    np.squeeze(np.argwhere(pt_bins_edges > 200))[0]: 0.02,
+    len(pt_bins_edges): 0.01,
+}
+unc_dict_neutrino = {
+    # get the index of pt_bin_edges where it is over 200 for the first time
+    np.squeeze(np.argwhere(pt_bins_edges > 200))[0]: 0.03,
+    np.squeeze(np.argwhere(pt_bins_edges > 300))[0]: 0.05,
+    len(pt_bins_edges): 0.01,
+}
+
+for i in range(len(jes)):
+    for j in range(len(jes[0])):
+        if "neutrino" not in args.version:
+            for key in unc_dict.keys():
+                if j < key:
+                    jes[i][j] = np.sqrt(jes[i][j] ** 2 + unc_dict[key] ** 2)
+                    break
+        else:
+            for key in unc_dict_neutrino.keys():
+                if j < key:
+                    jes[i][j] = np.sqrt(jes[i][j] ** 2 + unc_dict_neutrino[key] ** 2)
+                    break
+
+
+histo_2d = (jes, eta_bins_edges, pt_bins_edges)
+print(histo_2d)
+
+histo_dict={}
+for i in range(len(jes)):
+    histo_dict[f"({eta_bins_edges[i]} {eta_bins_edges[i+1]})"]={}
+    for j in range(len(jes[0])):
+        histo_dict[f"({eta_bins_edges[i]} {eta_bins_edges[i+1]})"][f"({pt_bins_edges[j]} {pt_bins_edges[j+1]})"]=jes[i][j] if not np.isnan(jes[i][j]) else -1
+#save the histo to json
+with open(f"textfiles/L2Res_{args.version}/L2Res_summary_{args.era}.json", "w") as f:
+    json.dump(histo_dict, f, indent=4)
+
+
+plot_2d_hist(histo_2d, f"L2Res_summary_{args.era}_SumUnc")
--- copy_files_to_workdir.py
+++ copy_files_to_workdir.py
@@ -9,10 +9,10 @@ work_dir=f"/work/mmalucch/L2L3Res_inputs/{args.version}/"
 
 #mkdir
 os.system(f"mkdir {work_dir}")
-os.system(f"mkdir {work_dir}/dijet")
-os.system(f"mkdir {work_dir}/gam")
+# os.system(f"mkdir {work_dir}/dijet")
+# os.system(f"mkdir {work_dir}/gam")
 os.system(f"mkdir {work_dir}/zb")
 
-os.system(f"cp ~/dijet/rootfiles/{args.version}/*root /work/mmalucch/L2L3Res_inputs/{args.version}/dijet/")
-os.system(f"cp ~/gamjet-analysis/rootfiles/{args.version}/*root /work/mmalucch/L2L3Res_inputs/{args.version}/gam/")
+# os.system(f"cp ~/dijet/rootfiles/{args.version}/*root /work/mmalucch/L2L3Res_inputs/{args.version}/dijet/")
+# os.system(f"cp ~/gamjet-analysis/rootfiles/{args.version}/*root /work/mmalucch/L2L3Res_inputs/{args.version}/gam/")
 os.system(f"cp ~/ZbAnalysis/figures/{args.version}*/jme_bplusZ_merged*.root /work/mmalucch/L2L3Res_inputs/{args.version}/zb/")
